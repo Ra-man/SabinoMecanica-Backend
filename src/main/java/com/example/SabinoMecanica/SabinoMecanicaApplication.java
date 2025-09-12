63e6ce256d30a152037c5de992ed85ff788dba7a
Rafael Pagnan <rafaelmarquespagnan@gmail.com>
2025-09-12 19:43:14 -0300

    Incializacao Spring

---
+++ src/main/java/com/example/SabinoMecanica/SabinoMecanicaApplication.java
@@ -0,0 +1,13 @@
+package com.example.SabinoMecanica;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SabinoMecanicaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SabinoMecanicaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/SabinoMecanica/SabinoMecanicaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.SabinoMecanica;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SabinoMecanicaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
