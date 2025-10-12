120e32e01c24c242b2c7e5b550180cbd40e1a82a
Rafael Pagnan <rafaelmarquespagnan@gmail.com>
2025-10-12 18:54:09 -0300

    Criação dos models

--- src/main/java/com/sabinomecanica/backend/models/MovtoEntrada.java
+++ src/main/java/com/sabinomecanica/backend/models/MovtoEntrada.java
@@ -1,4 +1,58 @@
 package com.sabinomecanica.backend.models;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+
+import java.util.Date;
+import java.util.UUID;
+
+@Entity
+@Table(name = "Movimento_Entrada ")
+@NoArgsConstructor
+@AllArgsConstructor
 public class MovtoEntrada {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private UUID id;
+
+    @ManyToOne
+    @JoinColumn(name = "id_servico", nullable = false)
+    private Servico servico;
+
+    private Date data;
+
+    private float valor_pago;
+
+    public UUID getId() {
+        return id;
+    }
+
+    public void setId(UUID id) {
+        this.id = id;
+    }
+
+    public Servico getServico() {
+        return servico;
+    }
+
+    public void setServico(Servico servico) {
+        this.servico = servico;
+    }
+
+    public Date getData() {
+        return data;
+    }
+
+    public void setData(Date data) {
+        this.data = data;
+    }
+
+    public float getValor_pago() {
+        return valor_pago;
+    }
+
+    public void setValor_pago(float valor_pago) {
+        this.valor_pago = valor_pago;
+    }
 }
